10c5442fd2e2edde1948de5bb098c44f6381d5bc
Siarhei Kandratsiuk <siarhei_kandratsiuk@epam.com>
2022-04-09 15:15:13 +0300

    increase amount of requests

--- src/MortgageApplicationQueueProcessor.js
+++ src/MortgageApplicationQueueProcessor.js
@@ -1,5 +1,4 @@
 const WrongDataException = require('./exceptions/WrongDataException');
-const NotEligibleForMortgageException = require('./exceptions/NotEligibleForMortgageException');
 
 class MortgageApplicationQueueProcessor {
     constructor(customerRepository) {
@@ -12,19 +11,7 @@ class MortgageApplicationQueueProcessor {
         if (customer === null)
             throw new WrongDataException(MortgageApplicationQueueProcessor.MESSAGE_INVALID_CUSTOMER);
 
-        if (this.isEligibleForMortgage(customer, amountRequested))
-            customer.updateBalance(amountRequested);
-        else
-            throw new NotEligibleForMortgageException();
-    }
-
-    isEligibleForMortgage(customer, amountRequested) {
-        let isEligibleForMortgage = false;
-
-        if (customer.badCreditHistoryCount === 0 && customer.balance > 0)
-            isEligibleForMortgage = customer.balance * 2 >= amountRequested;
-
-        return isEligibleForMortgage;
+        customer.processMortgage(amountRequested);
     }
 }
 
--- src/domain/Customer.js
+++ src/domain/Customer.js
@@ -1,3 +1,5 @@
+const NotEligibleForMortgageException = require("../exceptions/NotEligibleForMortgageException");
+
 module.exports = class Customer {
     constructor(id, firstName, lastName, balance, badCreditHistoryCount) {
         this.id = id;
@@ -9,5 +11,21 @@ module.exports = class Customer {
 
     updateBalance(amount) {
         this.balance += amount;
+    }
+
+    processMortgage(amountRequested) {
+        if (this.isEligibleForMortgage(amountRequested))
+            this.updateBalance(amountRequested);
+        else
+            throw new NotEligibleForMortgageException();
+    }
+
+    isEligibleForMortgage(amountRequested) {
+        let isEligibleForMortgage = false;
+
+        if (this.badCreditHistoryCount === 0 && this.balance > 0)
+            isEligibleForMortgage = this.balance * 2 >= amountRequested;
+
+        return isEligibleForMortgage;
     }
 }
